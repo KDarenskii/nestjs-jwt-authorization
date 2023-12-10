8d674a5baa152779c3508d051030e1b41cca6fca
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-11 00:45:42 +0300

    added port env and removed app service and controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), UsersModule, PrismaModule],
 })
 export class AppModule {}
