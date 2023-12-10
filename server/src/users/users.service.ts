dfb99c024c840cb78ec611e1d1206d80e1f5aaab
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-11 01:27:26 +0300

    added getByEmail method and changed create method

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async getByEmail(email: string): Promise<User | null> {
+    return this.prismaService.user.findUnique({ where: { email } });
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User | null> {
-    return this.prisma.user.create({ data: createUserDto });
+    const user = await this.getByEmail(createUserDto.email);
+
+    if (user) {
+      throw new BadRequestException("Email is already existing");
+    }
+
+    return this.prismaService.user.create({ data: createUserDto });
   }
 }
