217b7963e606e0e5e183291c3c4976c61c6a849d
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-13 01:08:08 +0300

    added access token to users provider and request to get users

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "@prisma/client";
+import { AccessTokenGuard } from "src/auth/guards";
 
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AccessTokenGuard)
+  @Get()
+  async get() {
+    return this.usersService.getAll();
+  }
+
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    console.log(createUserDto);
     return this.usersService.create(createUserDto);
   }
 }
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UsersController } from "./users.controller";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { AccessTokenStrategy } from "src/auth/strategies/access-token.strategy";
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AccessTokenStrategy],
   imports: [PrismaModule],
   exports: [UsersService],
 })
