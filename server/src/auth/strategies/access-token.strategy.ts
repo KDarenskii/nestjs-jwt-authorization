cf2a8c3c1389ae5a18cd5c839ccfc1f5aadbd1ae
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-13 01:04:12 +0300

    added access token strategy

---
+++ server/src/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "../types";
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(
+  Strategy,
+  "jwt-access-token",
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.ACCESS_TOKEN_SECRET,
+      ignoreExpiration: false,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
--- server/src/auth/strategies/index.ts
+++ server/src/auth/strategies/index.ts
@@ -1,1 +1,2 @@
 export * from "./refresh-token.strategy";
+export * from "./access-token.strategy";
