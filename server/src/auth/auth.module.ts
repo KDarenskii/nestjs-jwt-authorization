a166b26e7f8f0212c32f0d38a27340593ab07a7b
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-13 01:05:39 +0300

    added strategies to providers

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { UsersModule } from "src/users/users.module";
 import { JwtModule } from "@nestjs/jwt";
+import { RefreshTokenStrategy } from "./strategies";
+import { AccessTokenStrategy } from "./strategies";
 
 @Module({
   imports: [UsersModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
 })
 export class AuthModule {}
