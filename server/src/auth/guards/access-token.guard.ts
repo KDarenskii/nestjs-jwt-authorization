2db7452c99de0d01f8930f639a46be8e65f27354
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-14 16:51:03 +0300

    added public decorator to pass access token guard

--- server/src/auth/guards/access-token.guard.ts
+++ server/src/auth/guards/access-token.guard.ts
@@ -1,5 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
+import { Observable } from "rxjs";
+import { PUBLIC_METADATA_KEY } from "../decorators";
 
 @Injectable()
-export class AccessTokenGuard extends AuthGuard("jwt-access-token") {}
+export class AccessTokenGuard extends AuthGuard("jwt-access-token") {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(
+      PUBLIC_METADATA_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+}
