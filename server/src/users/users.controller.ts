aa34d911bef2f9dd4cf76822baa151131a4bd8b3
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-14 16:47:21 +0300

    removed access token guard

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Post, Body, Get, UseGuards } from "@nestjs/common";
+import { Controller, Post, Body, Get } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { User } from "@prisma/client";
-import { AccessTokenGuard } from "src/auth/guards";
 
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(AccessTokenGuard)
   @Get()
   async get() {
     return this.usersService.getAll();
