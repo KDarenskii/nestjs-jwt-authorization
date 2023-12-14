8adb75bb5526428a3fb3f1009895cc68d358ba1a
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-14 15:30:37 +0300

    configured global validation pipe

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from "@nestjs/common";
+import { Module, ValidationPipe } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
+import { APP_PIPE } from "@nestjs/core";
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule, PrismaModule, AuthModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ whitelist: true }),
+    },
+  ],
 })
 export class AppModule {}
