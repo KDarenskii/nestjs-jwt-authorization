d4e704c80795177471a0edd78f201d65d068c924
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-14 16:46:28 +0300

    configured global access token guard

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,7 +3,8 @@ import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
-import { APP_PIPE } from "@nestjs/core";
+import { APP_GUARD, APP_PIPE } from "@nestjs/core";
+import { AccessTokenGuard } from "./auth/guards";
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule, PrismaModule, AuthModule],
@@ -11,6 +12,10 @@ import { APP_PIPE } from "@nestjs/core";
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({ whitelist: true }),
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
     },
   ],
 })
