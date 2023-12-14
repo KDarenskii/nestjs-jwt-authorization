edcb593a8b586d2cec94f6dfca75ff6b099263f2
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-14 15:30:07 +0300

    added auth model validation

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+
 export class AuthDto {
+  @IsEmail(undefined, { message: "Invalid email" })
   email: string;
+
+  @IsString()
+  @MinLength(3)
   password: string;
 }
