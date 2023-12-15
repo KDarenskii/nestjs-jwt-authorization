4060b5e0a1d8e5ea249d004426ce97801dbc5b32
Kirill Darenskii <kdarenskii@mail.ru>
2023-12-15 12:51:27 +0300

    added global controllers prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from "cookie-parser";
 async function startApp() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.setGlobalPrefix("/api");
   await app.listen(process.env.PORT);
 }
 
